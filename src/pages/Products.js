2de14abb1a65617d6e3888d0341e85df6ea5d6a4
Yashagarwallaa <yashagarwalla001@gmail.com>
2024-04-03 18:49:22 +0530

    first-commit

---
+++ src/pages/Products.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Spotlight from '../components/Spotlight'
+
+function Products() {
+  return (
+    <div>
+        <Spotlight/>
+    </div>
+  )
+}
+
+export default Products
